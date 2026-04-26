5ded2557ab23990bad8f91294f0ed701fa28ce93
HellEXC <88082562+HellEXC@users.noreply.github.com>
2026-04-26 17:15:07 +0800

    Add git clone for nikki package
    Added a new git clone command for the nikki package.

--- diy.sh
+++ diy.sh
@@ -22,5 +22,6 @@ echo 'src-git webui https://github.com/lonelysix-editor/luci-app-webui.git;main'
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
 #git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
 git clone https://github.com/FUjr/QModem.git package/QModem
+git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
 #git clone https://github.com/lonelysix-editor/luci-app-webui.git package/webui
 #git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
