4225d7f87e4e00bfb026e20fc9d4576ae4472ef0
HellEXC <88082562+HellEXC@users.noreply.github.com>
2026-04-26 19:52:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,7 +18,8 @@ sed -i 's/LEDE/HellEXC/g' package/base-files/files/bin/config_generate
 #echo 'src-git webui https://github.com/lonelysix-editor/luci-app-webui.git;main' >>feeds.conf.default
 #echo 'src-git UA3F https://github.com/SunBK201/UA3F.git' >>feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
-git clone https://github.com/kenzok8/small.git package/small
+#git clone https://github.com/kenzok8/small.git package/small  
+git clone https://github.com/kenzok8/small-package.git package/small
 #git clone https://github.com/SunBK201/UA3F.git package/UA3F
 #git clone https://github.com/stevenjoezhang/luci-app-adguardhome.git package/ADGH
 git clone https://github.com/FUjr/QModem.git package/QModem
